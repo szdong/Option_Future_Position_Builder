29cf73ed2c65e272b76ce9eb53d2dfffd54173a5
szdong <szdoooooooong@gmail.com>
2020-09-04 15:18:07 +0900

    Added Move contract type.

--- position_builder.py
+++ position_builder.py
@@ -18,6 +18,14 @@ class future_info:
         self.lot = lot
 
 
+class move_info:
+    def __init__(self, strike_price, position_price, type: side, lot):
+        self.price = strike_price
+        self.position_price = position_price
+        self.type = type
+        self.lot = lot
+
+
 class spot_info:
     def __init__(self, price, lot):
         self.price = price
@@ -38,6 +46,7 @@ class position_builder:
         self.options_lst = []
         self.futures_lst = []
         self.spot_lst = []
+        self.move_lst = []
         self.x = []
         self.y = []
         self.symbol = symbol
@@ -51,9 +60,12 @@ class position_builder:
     def add_spot(self, spot_info: spot_info):
         self.spot_lst.append(spot_info)
 
+    def add_move(self, move_info: move_info):
+        self.move_lst.append(move_info)
+
     def build_position(self, scale=800, step=1, fee=0, size_x=10, size_y=6):
         price_lst = []
-        for contract in self.futures_lst + self.options_lst + self.spot_lst:
+        for contract in self.futures_lst + self.options_lst + self.spot_lst + self.move_lst:
             price_lst.append(contract.price)
 
         start = int(min(price_lst) - scale)
@@ -63,14 +75,19 @@ class position_builder:
             option_p = 0
             future_p = 0
             spot_p = 0
+            move_p = 0
+
             for option in self.options_lst:
-                option_p += (max(option.option_type * (s - option.price), 0) - option.premium) * option.lot * option.side
+                option_p += (max(option.option_type * (s - option.price),
+                                 0) - option.premium) * option.lot * option.side
             for future in self.futures_lst:
                 future_p += (future.future_type * (s - future.price) * future.lot)
             for spot in self.spot_lst:
                 spot_p += (s - spot.price) * spot.lot
+            for move in self.move_lst:
+                move_p += (abs(s - move.price) - move.position_price) * move.lot * move.type
 
-            p = option_p + future_p + spot_p - fee
+            p = option_p + future_p + spot_p + move_p - fee
 
             self.x.append(s)
             self.y.append(p)
